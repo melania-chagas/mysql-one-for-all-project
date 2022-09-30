51d89eac7b4e2991bfff121475eb4f8c0cd03358
Melania P Chagas <melaniapchagas@gmail.com>
2022-09-30 12:07:05 -0300

    Requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+SELECT
+  FORMAT(MIN(planos.preco_plano), 2) AS 'faturamento_minimo',
+  FORMAT(MAX(planos.preco_plano),2) AS 'faturamento_maximo',
+  FORMAT(AVG(planos.preco_plano), 2) AS 'faturamento_medio',
+  FORMAT(SUM(planos.preco_plano),2) AS 'faturamento_total'
+FROM
+	SpotifyClone.usuarios
+INNER JOIN SpotifyClone.planos
+  ON usuarios.plano_id = planos.plano_id;
